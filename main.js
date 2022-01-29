37b97c02a2f88547a9b2987db944fc90c6f7cce4
JyldyzKelechek <98655329+JyldyzKelechek@users.noreply.github.com>
2022-01-29 11:37:23 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+    // Пункт 1
+    let message = "ddd@bbb@ccc";
+    let newMessage = text.replaceAll("@", "!");
+    console.log(newMessage);
+
+    // Пункт 2
+    let message1 = 'js';
+    console.log(message1.toUpperCase());
+
+    // Пункт 3
+    let message2 ='JS';
+    console.log(message2.toLowerCase());
+
+    // Пункт 4
+    let message4 = 'I am a hero!';
+    console.log(message4.length);
+
+    // Пункт 5
+     let creditCard = prompt("Введите номер кредитной карты:");
+    let lastCreditCardNum = ("**********+numCard.slice(12)");
+    document.write(`Номер вашей карты: ${lastCreditCardNum}`);
+
+
